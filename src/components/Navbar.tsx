dbd8fc8fc5cd31784d62f19813a62ed03d975255
Marcos Ernesto Planos Valenzuela <87524702+Dantalian5@users.noreply.github.com>
2024-03-18 10:58:24 +0100

    fix Navbar
    - fix hover effect & time
    - add a more nyt luke like

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,56 +1,12 @@
-import { memo, useEffect, useRef, useReducer, useState } from "react";
+import { memo } from "react";
 import { Link } from "react-router-dom";
 import { svgArrow } from "@/assets/svgImg";
 import { navbarItems } from "@/assets/menuItems";
 
 const Navbar = memo(() => {
-  const [isSticky, setSticky] = useState(false);
-  const navbarRef = useRef<HTMLDivElement | null>(null);
-  const timeoutId = useRef<number | null>(null);
-
-  const handleObserver: IntersectionObserverCallback = (entries) => {
-    const [entry] = entries;
-    if (!entry.isIntersecting) {
-      // Inicia el temporizador cuando la barra de navegación sale de la vista
-      timeoutId.current = setTimeout(() => {
-        setSticky(true);
-      }, 200);
-    } else {
-      // Limpia el temporizador si la barra de navegación vuelve a entrar en la vista antes de que el temporizador expire
-      if (timeoutId.current) {
-        clearTimeout(timeoutId.current);
-        timeoutId.current = null;
-      }
-      setSticky(false);
-    }
-  };
-
-  useEffect(() => {
-    const referente = navbarRef.current;
-    const observer = new IntersectionObserver(handleObserver, {
-      threshold: [0],
-    });
-    if (referente) {
-      observer.observe(referente);
-    }
-
-    return () => {
-      if (referente) {
-        observer.unobserve(referente);
-      }
-      // Asegúrate de limpiar el temporizador cuando el componente se desmonte
-      if (timeoutId.current) {
-        clearTimeout(timeoutId.current);
-      }
-    };
-  }, [navbarRef]);
-
-  console.log(isSticky);
-
   return (
     <nav
-      ref={navbarRef}
-      className={`top-0 z-10 mx-auto mb-1  hidden max-w-[1285px] bg-white px-5 lg:block lg:px-11 ${isSticky ? "fixed" : "static"}`}
+      className={` sticky top-0 z-10 mx-auto mb-1  hidden max-w-[1285px] bg-white px-5 lg:block lg:px-11 `}
     >
       <ul className="flex items-center justify-center border-y border-b-black-100 border-t-gray-200">
         {Object.entries(navbarItems).map(([section, sectionInfo]) => (
@@ -69,14 +25,14 @@ const Navbar = memo(() => {
                 {section}
               </span>
             </Link>
-
-            <span className={`ml-1 cursor-pointer text-base text-gray-300`}>
-              {svgArrow}
-            </span>
+            <span className={`ml-1 text-lg text-gray-300`}>{svgArrow}</span>
             <div
-              className={`pointer-events-none absolute left-1/2 top-full min-h-20 w-dvw -translate-x-1/2 gap-x-16 bg-white opacity-0 shadow-menu transition-opacity duration-500 ease-out group-hover:pointer-events-auto group-hover:opacity-100`}
+              className={`pointer-events-none absolute left-1/2 top-[calc(100%-1px)] w-dvw -translate-x-1/2 gap-x-16 bg-white opacity-0 shadow-menu transition-opacity duration-500 ease-out group-hover:pointer-events-auto group-hover:opacity-100`}
             >
-              <div className="mx-auto flex w-full max-w-[1285px] gap-x-20 bg-white px-5 pb-10 pt-6 lg:px-11">
+              <span className="mx-auto block w-full max-w-[1285px] gap-x-20  px-5 lg:px-11 ">
+                <span className="block w-full border-t border-t-gray-200"></span>
+              </span>
+              <div className="mx-auto flex w-full max-w-[1285px] gap-x-20 bg-white px-5 pb-10 pt-6 lg:px-11 ">
                 {Object.entries(sectionInfo.subsection).map(
                   ([subsection, subsectionList]) => (
                     <div className={``} key={subsection}>
